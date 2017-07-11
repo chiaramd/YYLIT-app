79ceccfa612bfd60853b491aee0272f142593594
BrianaH <hutchinsonbriana12@yahoo.com>
2017-07-11 11:19:59 -0400

    changes to manifest and strings.xml

--- app/src/main/java/com/picture/PhotoActivity.java
+++ app/src/main/java/com/picture/PhotoActivity.java
@@ -2,6 +2,8 @@ package com.picture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.gwc.yylit.R;
 
@@ -11,6 +13,12 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PhotoActivity.this,
+          android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 }
 
