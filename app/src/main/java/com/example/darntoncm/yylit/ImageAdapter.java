7b4f2666bca3349445e92637c806fca3ebe31c4d
chiaramd <chiara.darnton@gmail.com>
2017-07-11 15:05:06 -0400

    gridView with pictures from drawables

---
+++ app/src/main/java/com/example/darntoncm/yylit/ImageAdapter.java
@@ -0,0 +1,48 @@
+package com.example.darntoncm.yylit;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.example.gwc.yylit.R;
+
+/**
+ * Created by darntoncm on 7/11/17.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+    public int getCount() {
+        return mThumbIds.length;
+    }
+    public Object getItem(int position) {
+        return null;
+    }
+    public long getItemId(int position) {
+        return 0;
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView==null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85,85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8,8,8,8);
+        } else {
+            imageView=(ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    private Integer[] mThumbIds = {
+            R.drawable.hunger1, R.drawable.hunger2
+    };
+}
--- app/src/main/java/com/example/darntoncm/yylit/MainActivity.java
+++ app/src/main/java/com/example/darntoncm/yylit/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.darntoncm.yylit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.gwc.yylit.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
