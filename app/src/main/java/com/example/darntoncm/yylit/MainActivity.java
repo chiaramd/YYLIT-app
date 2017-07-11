fb03cbd2d659d243d0cfa1d7be2f595edd3e74dd
Ellie Wangerin <elliewangerine@gmail.com>
2017-07-11 15:45:02 -0400

    buggy

--- app/src/main/java/com/example/darntoncm/yylit/MainActivity.java
+++ app/src/main/java/com/example/darntoncm/yylit/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.darntoncm.yylit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.gwc.yylit.R;
+
+import static com.example.gwc.yylit.R.id.btnCommute;
+import static com.example.gwc.yylit.R.id.btnFamily;
+import static com.example.gwc.yylit.R.id.btnTecnology;
+import static com.example.gwc.yylit.R.id.btnWork;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +18,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        Intent changeScreen = new Intent(MainActivity.this, SelectionActivity.class);
+        changeScreen.putExtra("title_string", "Choice");
+
+        Button btnFood = (Button) findViewById(R.id.btnFood);
+        btnFood.setOnClickListener(new btnFood.onClickListener());{
+                String Choice = "Food";
+                String title_string = "foodChoice";
+                startActivity(changeScreen);
+            }
+        }
+
+        Button btnWork = (Button) findViewById(R.id.btnWork);
+        btnWork.setOnClickListener(new btnWork.onClickListener());{
+                String Choice = "Work";
+                String title_string = "workChoice";
+                startActivity(changeScreen);
+            }
+        }
+
+        Button btnTechnology = (Button) findViewById(R.id.btnTecnology);
+        btnTechnology.setOnClickListener(new btnTechnology.onClickListener());{
+            public void onClick();{
+                String Choice = "Technology";
+                String title_string = "technologyChoice";
+                startActivity(changeScreen);
+            }
+        }
+
+        Button btnFamily = (Button) findViewById(R.id.btnFamily);
+        btnFamily.setOnClickListener(new btnFamily.onClickListener());{
+            public void onClick();{
+                String Choice = "Family";
+                String title_string = "familyChoice";
+                startActivity(changeScreen);
+            }
+        }
+
+        Button btnCommute = (Button) findViewById(R.id.btnCommute);
+        btnCommute.setOnClickListener(new btnCommute.onClickListener());{
+            public void onClick();{
+                String Choice = "Commute";
+                String title_string = "commuteChoice";
+                startActivity(changeScreen);
+            }
+        }
+
     }
+
 
 }
--- app/src/main/java/com/example/darntoncm/yylit/SelectionActivity.java
+++ app/src/main/java/com/example/darntoncm/yylit/SelectionActivity.java
@@ -1,9 +1,13 @@
 package com.example.darntoncm.yylit;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +20,7 @@ public class SelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
 
         Button btnRandomGenerate = (Button) (findViewById(R.id.btnRandomGenerate));
         btnRandomGenerate.setOnClickListener(new View.OnClickListener() {
@@ -39,4 +44,5 @@ public class SelectionActivity extends AppCompatActivity {
         });
 
     }
+
 }
