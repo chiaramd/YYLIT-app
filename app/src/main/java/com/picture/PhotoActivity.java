9767f0e32a8d1f2d8342bc331bf3d72006643393
chiaramd <chiara.darnton@gmail.com>
2017-07-14 14:10:41 -0400

    context menu

--- app/src/main/java/com/picture/PhotoActivity.java
+++ app/src/main/java/com/picture/PhotoActivity.java
@@ -1,20 +1,37 @@
 package com.picture;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.gwc.yylit.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 import static com.example.gwc.yylit.R.id.imageView;
 
 public class PhotoActivity extends AppCompatActivity {
-
+    Bitmap anImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,26 +46,104 @@ public class PhotoActivity extends AppCompatActivity {
 
 
         Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), variable);
+        anImage = ((BitmapDrawable) drawable).getBitmap();
 
 
         myImage.setImageDrawable(drawable);
 
+        registerForContextMenu(myImage);
 
-
-
-
-
-
-
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
-
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PhotoActivity.this,
-          android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
-
+//        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+//
+//        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PhotoActivity.this,
+//          android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+//        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        mySpinner.setAdapter(myAdapter);
 
     }
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+    }
+    public void OnContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.save_to_gallery:
+                saveImageToExternalStorage(anImage);
+                Toast.makeText(getApplicationContext(), "Saved successfully, Check gallery", Toast.LENGTH_SHORT).show();
+                break;
+//            case R.id.send_to_friends:
+//                deleteNote(info.id);
+        }
+    }
+//    public void saveImageToExternal(String imgName, Bitmap bm) throws IOException {
+////Create Path to save Image
+//        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES+appFolder); //Creates app specific folder
+//        path.mkdirs();
+//        File imageFile = new File(path, imgName+".png"); // Imagename.png
+//        FileOutputStream out = new FileOutputStream(imageFile);
+//        try{
+//            bm.compress(Bitmap.CompressFormat.PNG, 100, out); // Compress Image
+//            out.flush();
+//            out.close();
+//
+//            // Tell the media scanner about the new file so that it is
+//            // immediately available to the user.
+//            MediaScannerConnection.scanFile(context,new String[] { imageFile.getAbsolutePath() }, null,new MediaScannerConnection.OnScanCompletedListener() {
+//                public void onScanCompleted(String path, Uri uri) {
+//                    Log.i("ExternalStorage", "Scanned " + path + ":");
+//                    Log.i("ExternalStorage", "-> uri=" + uri);
+//                }
+//            });
+//        } catch(Exception e) {
+//            throw new IOException();
+//        }
+//
+//
+//    }
+
+    private void saveImageToExternalStorage(Bitmap finalBitmap) {
+        String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+        File myDir = new File(root + "/saved_images_1");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-" + n + ".jpg";
+        File file = new File(myDir, fname);
+        if (file.exists())
+            file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // Tell the media scanner about the new file so that it is
+        // immediately available to the user.
+        MediaScannerConnection.scanFile(this, new String[]{file.toString()}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("ExternalStorage", "Scanned " + path + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    }
+                });
+    }
+
+
+
+
+
+
+
 }
 
 
