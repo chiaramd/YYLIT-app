fae6fa6d7ffd05be9c254dcf264fc353cabc8235
chiaramd <chiara.darnton@gmail.com>
2017-07-14 15:14:12 -0400

    add to gallery functional

--- app/src/main/java/com/picture/PhotoActivity.java
+++ app/src/main/java/com/picture/PhotoActivity.java
@@ -1,12 +1,16 @@
 package com.picture;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +25,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.darntoncm.yylit.MainActivity;
 import com.example.gwc.yylit.R;
 
 import java.io.File;
@@ -34,8 +39,12 @@ public class PhotoActivity extends AppCompatActivity {
     Bitmap anImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+//        Log.d("TAG24", "activity started");
+
+
 
         ImageView myImage = (ImageView) findViewById(R.id.imageView);
 
@@ -50,6 +59,7 @@ public class PhotoActivity extends AppCompatActivity {
 
 
         myImage.setImageDrawable(drawable);
+
 
         registerForContextMenu(myImage);
 
@@ -66,19 +76,30 @@ public class PhotoActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
+//        Log.d("TAG22", "menu created");
 
     }
-    public void OnContextItemSelected(MenuItem item) {
+    public boolean onContextItemSelected(MenuItem item) {
+//        Log.d("TAG23", "item selected");
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 
+
+
+//        Log.d("TAG24", "info retrieved");
         switch (item.getItemId()) {
             case R.id.save_to_gallery:
-                saveImageToExternalStorage(anImage);
-                Toast.makeText(getApplicationContext(), "Saved successfully, Check gallery", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(PhotoActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
+
+//                Log.d("TAG21", "case1");
+
                 break;
-//            case R.id.send_to_friends:
+            case R.id.send_to_friends:
+                break;
 //                deleteNote(info.id);
         }
+        return super.onContextItemSelected(item);
     }
 //    public void saveImageToExternal(String imgName, Bitmap bm) throws IOException {
 ////Create Path to save Image
@@ -105,37 +126,62 @@ public class PhotoActivity extends AppCompatActivity {
 //
 //
 //    }
+@Override
+public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+    switch (requestCode) {
+        case 1: {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission granted and now can proceed
+                saveImageToExternalStorage(anImage);
+                Toast.makeText(getApplicationContext(), "Saved successfully. Check gallery.", Toast.LENGTH_SHORT).show();
+            } else {
 
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Toast.makeText(PhotoActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        // add other cases for more permissions
+    }
+}
     private void saveImageToExternalStorage(Bitmap finalBitmap) {
-        String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
-        File myDir = new File(root + "/saved_images_1");
-        myDir.mkdirs();
+//        Log.d("TAG20", "saveImageToExternalStorage");
+//        String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+//        File myDir = new File(root + "/saved_images_1");
+//        myDir.mkdirs();
         Random generator = new Random();
         int n = 10000;
         n = generator.nextInt(n);
         String fname = "Image-" + n + ".jpg";
-        File file = new File(myDir, fname);
-        if (file.exists())
-            file.delete();
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Log.d("TAG30", fname);
+//        File file = new File(myDir, fname);
+//        if (file.exists())
+//            file.delete();
+//        try {
+//            FileOutputStream out = new FileOutputStream(file);
+//            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+//            out.flush();
+//            out.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        String description = "this is a description";
+        MediaStore.Images.Media.insertImage(getContentResolver(), anImage, fname, description);
+
 
 
         // Tell the media scanner about the new file so that it is
         // immediately available to the user.
-        MediaScannerConnection.scanFile(this, new String[]{file.toString()}, null,
-                new MediaScannerConnection.OnScanCompletedListener() {
-                    public void onScanCompleted(String path, Uri uri) {
-                        Log.i("ExternalStorage", "Scanned " + path + ":");
-                        Log.i("ExternalStorage", "-> uri=" + uri);
-                    }
-                });
+//        MediaScannerConnection.scanFile(this, new String[]{file.toString()}, null,
+//                new MediaScannerConnection.OnScanCompletedListener() {
+//                    public void onScanCompleted(String path, Uri uri) {
+//                        Log.i("ExternalStorage", "Scanned " + path + ":");
+//                        Log.i("ExternalStorage", "-> uri=" + uri);
+//                    }
+//                });
     }
 
 
